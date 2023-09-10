98773a5d99d4a78284bad89a639efff73407553a
Vinuta2003 <bhoomikabhat2003@gmail.com>
2023-09-10 14:35:42 +0530

    schedule fetched from backend

--- sample_data.js
+++ sample_data.js
@@ -1,23 +1,33 @@
-var express=require('express');
-var app=express();
-const PORT = process.env.PORT || 3000
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
 
-var router=express.Router();
-app.set("view engine", "ejs");
-
-var database=require('./views/database.js');
-router.get("/",function(request,response,next){
-    var query="SELECT * from schedule";
-    database.query(query,function(error,data){
-        if (error) {throw error;}
-        else{
-            response.render('sample_data',{title:'Node.js MySQL',action:'list',sampleData:data});
-        }
-    })
+const connection = mysql.createConnection({
+  host: 'localhost', // e.g., localhost
+  user: 'root',
+  password: '',
+  database: 'railways',
 });
 
-router.get("/add",function(request,response,next){
-    response.send('add sample data');
-})
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to database');
+});
 
-app.listen(PORT);
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM schedule', (err, rows) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Error fetching data');
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
